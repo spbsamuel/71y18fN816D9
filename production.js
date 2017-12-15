8d4e75d8d1e232845fcb18e699abaa27956d1768
samuel <spbsamuel@gmail.com>
2017-12-16 01:37:52 +0800

    init

---
+++ production.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const ServerRendererPath = path.join(__dirname, './dist/server.js');
+const ServerRenderer = require(ServerRendererPath).default;
+
+app.use('/dist', express.static(path.join(__dirname, './dist')));
+
+app.use(ServerRenderer({}));
+
+app.listen(3000);
