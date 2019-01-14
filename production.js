a84bd5a8877f889b3cc575f9b3791e6fd5e8b4c6
samuel <spbsamuel@gmail.com>
2019-01-13 21:56:26 -0500

    undo wrong favicon changes

--- production.js
+++ production.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 const app = express();
 
 const ServerRendererPath = path.join(__dirname, './dist/server.js');
 const ServerRenderer = require(ServerRendererPath).default;
+
+app.get('/robots.txt', function (req, res) {
+  res.type('text/plain');
+  res.send("User-agent: *\nDisallow: /");
+});
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 app.use('/dist', express.static(path.join(__dirname, './dist')));
 
